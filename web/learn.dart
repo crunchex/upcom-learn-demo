fa25295745d0a034b975b2216926fa8ad049e8f0
Mike Lewis <mike@updroid.com>
2015-08-05 22:21:40 -0700

    web: added up1 image

--- web/learn.dart
+++ web/learn.dart
@@ -16,13 +16,31 @@ class UpDroidLearn extends TabController {
     return menu;
   }
 
+  DivElement containerDiv;
+  ImageElement image;
+  int _width = 667;
+  int _height = 527;
+
   UpDroidLearn() :
-  super(UpDroidLearn.names, getMenuConfig(), 'tabs/upcom-console/console.css') {
+  super(UpDroidLearn.names, getMenuConfig(), 'tabs/upcom-learn-demo/learn.css') {
 
   }
 
   void setUpController() {
+    // Dummy div so that teleop fits with the onFocus API from tab controller.
+    containerDiv = new DivElement()
+      ..classes.add('upcom-learn-container');
+    view.content.children.add(containerDiv);
 
+    view.content.contentEdge.height = new Dimension.percent(100);
+
+    // TODO: compress this svg (use that OS X tool).
+    image = new ImageElement(src:'tabs/$refName/updroid-onearm.png')
+      ..style.position = 'absolute'
+      ..style.top = '50%'
+      ..style.right = '0'
+      ..style.transform = 'translateY(-50%)';
+    containerDiv.children.add(image);
   }
 
   void registerMailbox() {
@@ -30,7 +48,18 @@ class UpDroidLearn extends TabController {
   }
 
   void registerEventHandlers() {
+    window.onResize.listen((e) => _setDimensions());
+  }
 
+  void _setDimensions() {
+    var width = (view.content.contentEdge.width - 13);
+
+    if (width <= _width) {
+      image.width = width;
+    } else {
+      image.width = _width;
+    }
+    image.height = (image.width * _height / _width).toInt();
   }
 
   Element get elementToFocus => null;
